23642b6022928eccb416e63aef95eee7145e5593
City7g <City7gor@gmail.com>
2023-07-26 21:01:30 +0300

    Add navigation to slider

--- components/Slider.vue
+++ components/Slider.vue
@@ -7,10 +7,17 @@
     :autoplay="{
       delay: 4000
     }"
+    :navigation="{
+      nextEl: '.fff',
+      prevEl: '.ggg'
+    }"
   >
     <SwiperSlide v-for="slide in 10" :key="slide" class="slider__item">
       <img src="/img/slider-cat.svg" class="slider__img" alt="">
     </SwiperSlide>
+
+    <div class="fff">1</div>
+    <div class="ggg">2</div>
   </Swiper>
 </template>
 
